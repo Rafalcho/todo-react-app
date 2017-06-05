f41bcf6bc102bfde9160974f7cb3a211020f4b11
Rafalcho <raf.cholaj@gmail.com>
2017-06-05 13:11:33 +0200

    deletetodo added

--- src/utils/actions.js
+++ src/utils/actions.js
@@ -1,7 +1,15 @@
-export const addTodo = (todoItems, newTodo) => {
-  return [...todoItems, newTodo];
-};
+export const addTodo = (todoItems, newTodo) => (
+  [...todoItems, newTodo]
+);
 
 export const generateNumber = () => (
   Math.floor(Math.random() * 10000) + 1
 );
+
+export const deleteTodo = (todoItems, id) => {
+  const removeId = todoItems.findIndex(item => item.id === id);
+  return [
+    ...todoItems.slice(0, removeId),
+    ...todoItems.slice(removeId + 1)
+    ];
+};
--- src/utils/actions.test.js
+++ src/utils/actions.test.js
@@ -1,4 +1,4 @@
-import {addTodo} from './actions.js';
+import {addTodo, deleteTodo} from './actions.js';
 
 test('addTodo should add new todo to the array', () => {
   const oldArray = [
@@ -35,5 +35,38 @@ test('addTodo should not change exisitng array', () => {
 
   const result = addTodo(oldArray, newTodo);
 
-  expect(result).not.toBe(expected);
+  expect(result).not.toBe(oldArray);
+});
+
+test('deleteTodo should remove todo from the array by given id', () => {
+  const oldArray = [
+    {id: 1, name: 'first', isDone: false},
+    {id: 2, name: 'second', isDone: false},
+    {id: 3, name: 'third', isDone: false}
+  ];
+
+  const id = 1;
+
+  const expected = [
+    {id: 2, name: 'second', isDone: false},
+    {id: 3, name: 'third', isDone: false}
+  ];
+
+  const result = deleteTodo(oldArray, id);
+
+  expect(result).toEqual(expected);
+});
+
+test('deleteTodo should not change exisitng array', () => {
+  const oldArray = [
+    {id: 1, name: 'first', isDone: false},
+    {id: 2, name: 'second', isDone: false},
+    {id: 3, name: 'third', isDone: false},
+  ];
+
+  const id = 1;
+
+  const result = deleteTodo(oldArray, id);
+
+  expect(result).not.toBe(oldArray);
 });
